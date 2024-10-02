b8dadea429e083fbdb281f5769d5e78134b95d9c
shantanushrivastava <shantanushrivastava001@gmail.com>
2024-10-02 17:07:25 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 10, num2 = 20, sum;
+
+    // Add the numbers
+    sum = num1 + num2;
+
+    // Output the result
+    cout << "The sum of 10 and 20 is: " << sum << endl;
+
+    return 0;
+}
