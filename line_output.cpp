80d382f52ee782dd105f4f663fe0603f7933bddd
shantanushrivastava <shantanushrivastava001@gmail.com>
2024-10-07 09:50:04 +0530

    Add files via upload

---
+++ line_output.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cout<<"enter your line ";
+    getline(cin,str);
+    cout<<"i want to print : "<< str;
+    return 0;
+}
